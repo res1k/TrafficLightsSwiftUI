5393a54992ae7d2dcf3e85f9052b171d7c82940a
Andrey Zhivotov <res_a@mail.ru>
2022-05-13 23:19:21 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Andrey Zhivotov on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
